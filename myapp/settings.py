767749267f94a18545f2a37073102b3a1a53c26e
AliSaif110-A <125074821+AliSaif110-A@users.noreply.github.com>
2023-04-16 14:36:07 +0100

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
+
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
     }
+
 }
 
 
@@ -118,7 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
